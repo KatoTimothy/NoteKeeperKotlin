c682d2c19cfa750d8baee1ecc9803d2e7d6913f2
KatoTimothy <40914849+KatoTimothy@users.noreply.github.com>
2021-01-15 20:38:30 +0300

    Update NoteListFragment.kt

--- app/src/main/java/com/android/mctimkato/notekeeper/notelist/NoteListFragment.kt
+++ app/src/main/java/com/android/mctimkato/notekeeper/notelist/NoteListFragment.kt
@@ -1,9 +1,8 @@
 package com.android.mctimkato.notekeeper.notelist
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -12,6 +11,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.android.mctimkato.notekeeper.data.DataManager
 import com.android.mctimkato.notekeeper.R
+import com.android.mctimkato.notekeeper.SettingsActivity
 import com.android.mctimkato.notekeeper.databinding.FragmentNoteListBinding
 
 class NoteListFragment : Fragment() {
@@ -56,6 +56,28 @@ class NoteListFragment : Fragment() {
             val action = NoteListFragmentDirections.actionNavNotesToNoteFragment()
             fabView.findNavController().navigate(action)
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                val intent = Intent(requireContext(), SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+
+        }
+
+    }
+
 }
