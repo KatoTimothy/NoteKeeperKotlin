692eca605c444c17437ae9be8c55078d8c3b8e87
KatoTimothy <40914849+KatoTimothy@users.noreply.github.com>
2021-03-05 17:21:59 +0300

    Update Entities.kt
    Added the title field to the Note entity

--- app/src/main/java/com/android/mctimkato/notekeeper/database/entities/Entities.kt
+++ app/src/main/java/com/android/mctimkato/notekeeper/database/entities/Entities.kt
@@ -1,18 +1,26 @@
 package com.android.mctimkato.notekeeper.database.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /*
 * Represents a single entry of note in the database
 * */
+@Entity(tableName = "notes")
 data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val courseId: String,
+    val title: String,
     val summary: String
 )
 
 /*
 * Represents a single entry of course in the database
 * */
+@Entity(tableName = "courses")
 data class CourseEntity(
+    @PrimaryKey
     val id: String,
     val title: String
 )
