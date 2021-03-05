2ba98898949908f7065d3da304db68f69b09c0ae
KatoTimothy <40914849+KatoTimothy@users.noreply.github.com>
2021-03-06 02:07:43 +0300

    Added sample Note objects for testing

--- app/src/androidTest/java/com/android/mctimkato/notekeeper/NoteKeeperDatabaseTest.kt
+++ app/src/androidTest/java/com/android/mctimkato/notekeeper/NoteKeeperDatabaseTest.kt
@@ -4,16 +4,22 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.android.mctimkato.notekeeper.room.NotekeeperDatabase
 import com.android.mctimkato.notekeeper.room.dao.CoursesDao
 import com.android.mctimkato.notekeeper.room.dao.NotesDao
 import com.android.mctimkato.notekeeper.room.entities.Note
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
-import java.lang.Exception
 
 @RunWith(AndroidJUnit4::class)
 class NoteKeeperDatabaseTest {
@@ -23,17 +29,30 @@ class NoteKeeperDatabaseTest {
 
     private lateinit var database: NotekeeperDatabase
 
+    private val firstNote = Note(
+        1, "android_test",
+        "Unit Testing",
+        "It is really important to do unit test"
+    )
+    private val secondNote = Note(
+        2,
+        "kotlin_coroutines",
+        "Kotlin Coroutines",
+        "They enable handling asynchronous tasks"
+    )
+
     @Before
     fun createDatabase() {
         //get application context
         val context = ApplicationProvider.getApplicationContext<Context>()
 
         //create an in memory database
-        database = Room.inMemoryDatabaseBuilder(context, NotekeeperDatabase::class.java).build()
+        database = Room.inMemoryDatabaseBuilder(context, NotekeeperDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
 
         notesDao = database.notesDao
         coursesDao = database.courseDao
-
     }
 
     @After
@@ -44,16 +63,36 @@ class NoteKeeperDatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    suspend fun addNoteAndGetNoteFromList() {
-        val note = Note(1, "android_test", "Unit Testing", "It is really important to do unit test")
-        val note2 = Note(
-            2,
-            "kotlin_coroutines",
-            "Kotlin Coroutines",
-            "They enable handling asynchronous tasks"
-        )
+    fun insertingOneNoteReturnsThatSingleNote() {
 
-        val rowsAffected = notesDao.putVarying(note, note2)
+        CoroutineScope(Dispatchers.IO).launch {
 
+            notesDao.put(firstNote)
+
+            val testNote = notesDao.get(1)
+
+            testNote.collect {
+//                assertEquals(it, firstNote)
+//                assertEquals(firstNote.courseId, it.courseId)
+                assertEquals(it.courseId, 1)
+            }
+        }
+
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertingVariableListofTwoNotesRetrieveTwoNotesBack() {
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            notesDao.putVariableListofNotes(firstNote, secondNote)
+
+            notesDao.getAll().collect {
+
+                assertEquals(it.size, 1)
+            }
+
+        }
     }
 }
