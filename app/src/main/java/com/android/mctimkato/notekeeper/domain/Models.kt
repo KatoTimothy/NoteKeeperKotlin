b88985284d25371a79c47262c5cb555f3001eb7c
KatoTimothy <40914849+KatoTimothy@users.noreply.github.com>
2021-03-05 15:49:08 +0300

    Update Models.kt
    'Module' was more meaningful than summary for a Note

--- app/src/main/java/com/android/mctimkato/notekeeper/domain/Models.kt
+++ app/src/main/java/com/android/mctimkato/notekeeper/domain/Models.kt
@@ -1,10 +1,11 @@
 package com.android.mctimkato.notekeeper.domain
 
 /**
- * This contains information about the note to be displayed in the UI*/
+ * This contains information about the note to be displayed in the UI
+ * */
+
 data class Note(
-    val id : Int,
-    val course: String,
-    val module: String,
+    val id: Int,
+    val courseId: String,
     val summary: String
 )
