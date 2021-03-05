db4d01b48ae8a570715fcd200ba09df16d7bfa87
KatoTimothy <40914849+KatoTimothy@users.noreply.github.com>
2021-03-05 17:22:20 +0300

    Added the title field

--- app/src/main/java/com/android/mctimkato/notekeeper/domain/Models.kt
+++ app/src/main/java/com/android/mctimkato/notekeeper/domain/Models.kt
@@ -3,9 +3,9 @@ package com.android.mctimkato.notekeeper.domain
 /**
  * This contains information about the note to be displayed in the UI
  * */
-
 data class Note(
     val id: Int,
     val courseId: String,
+    val title: String,
     val summary: String
 )
